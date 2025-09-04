f8d56d4fa2424e024e7b37dd0bb37a766ee1891f
Amaan63 <amaansayed63@gmail.com>
2025-09-04 20:04:40 +0530

    generated Token when user log in

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,15 @@
 import loginUser from "../services/auth.service.js";
+import { generateToken } from "../utils/jwt.js"; // <-- import token generator
 
 const login = async (req, res) => {
   const { username, password } = req.body;
+
   try {
     const user = await loginUser(username, password);
+
+    // Generate JWT token
+    const token = generateToken(user);
+
     res.status(200).json({
       success: true,
       message: "Login successful",
@@ -11,6 +17,7 @@ const login = async (req, res) => {
         id: user.id,
         username: user.username,
       },
+      generatedToken: token, // <-- send token to frontend
     });
   } catch (error) {
     res.status(401).json({ success: false, message: error.message });
