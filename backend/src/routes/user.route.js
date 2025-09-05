10fae505b1c9a4ec3db2eb232835dcf054b2ccc3
Amaan63 <amaansayed63@gmail.com>
2025-09-05 23:38:41 +0530

    added category user route to get the categories

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { getProfile } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import categoryUserRoute from "./user/category.user.route.js";
 
 const userRoute = express.Router();
 
 userRoute.get("/profile", authMiddleware, getProfile);
+userRoute.use("/", categoryUserRoute); // GET /api/users/profile
 
 export default userRoute;
