23d811a0ef4dd8d4792ecaa06931e7cd2cf0666a
Amaan63 <amaansayed63@gmail.com>
2025-09-05 23:38:20 +0530

    updated the Admin route path and name

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -2,8 +2,8 @@ import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { getAllUsersController } from "../controllers/admin.controller.js";
 import { authorizeRoles } from "../middlewares/authorize.role.js";
-import categoryRoute from "./category.route.js";
 import { ROLES } from "../utils/roles.js";
+import categoryAdminRoute from "./admin/category.admin.route.js";
 
 const adminRoute = express.Router();
 
@@ -13,6 +13,6 @@ adminRoute.get(
   authorizeRoles(ROLES.ADMIN),
   getAllUsersController
 );
-adminRoute.use("/Category", categoryRoute);
+adminRoute.use("/Category", categoryAdminRoute);
 
 export default adminRoute;
