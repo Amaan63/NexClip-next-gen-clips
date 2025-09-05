c542f77a67a592898354b4bfe42a67f5c54b561b
Amaan63 <amaansayed63@gmail.com>
2025-09-05 20:32:59 +0530

    added user route

--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authRoute from "./auth.route.js";
-
+import userRoute from "./user.route.js";
 const router = express.Router();
 
 // Auth route
 router.use("/auth", authRoute);
+// User route
+router.use("/user", userRoute);
 
 export default router;
