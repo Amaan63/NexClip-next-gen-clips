fac4c87912efe20a514d1d601b289df513920e89
Amaan63 <amaansayed63@gmail.com>
2025-09-05 19:53:02 +0530

    called seed admin after connecting to DB

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,15 @@ import dotenv from "dotenv";
 import ConnectDB from "./src/config/db.js";
 import app from "./src/app.js";
 import seedUser from "./src/utils/seedUser.js";
+import seedAdmin from "./src/utils/seedAdmin.js";
 
 // configuring dotnev
 dotenv.config();
 
 // Connecting to Database
 ConnectDB().then(() => {
-  // seedUser(); to add a default user
+  //seedUser(); //to add a default user
+  //seedAdmin(); // to add a default admin
 });
 
 // Getting the Port from the env
