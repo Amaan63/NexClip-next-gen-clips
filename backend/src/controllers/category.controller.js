15dd3c7ea92db55c587b0355babdca8b65e834bb
Amaan63 <amaansayed63@gmail.com>
2025-09-05 23:54:03 +0530

    added UpdateCategory Controller

--- backend/src/controllers/category.controller.js
+++ backend/src/controllers/category.controller.js
@@ -1,4 +1,9 @@
-import { createCategory, getAllCategories } from "../services/category.service.js";
+import {
+  createCategory,
+  deleteCategoryById,
+  getAllCategories,
+  updateCategoryById,
+} from "../services/category.service.js";
 
 export const createCategoryController = async (req, res) => {
   try {
@@ -33,3 +38,22 @@ export const getAllCategoriesController = async (req, res) => {
     return res.status(404).json({ success: false, message: error.message });
   }
 };
+
+// Update Category
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { categoryId } = req.params;
+    const { name, description, avatarUrl } = req.body;
+
+    const updatedCategory = await updateCategoryById(categoryId, {
+      name,
+      description,
+      avatarUrl,
+    });
+
+    res.status(200).json({ success: true, data: updatedCategory });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
