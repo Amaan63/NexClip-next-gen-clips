47eb27e2f724c60d8249d9db53d5a14e3f0f9e00
Amaan63 <amaansayed63@gmail.com>
2025-09-05 23:06:39 +0530

    Added category Rute in Admin Route

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -2,14 +2,17 @@ import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { getAllUsersController } from "../controllers/admin.controller.js";
 import { authorizeRoles } from "../middlewares/authorize.role.js";
+import categoryRoute from "./category.route.js";
+import { ROLES } from "../utils/roles.js";
 
 const adminRoute = express.Router();
 
 adminRoute.get(
   "/getAllUsers",
   authMiddleware,
-  authorizeRoles("admin"),
+  authorizeRoles(ROLES.ADMIN),
   getAllUsersController
 );
+adminRoute.use("/Category", categoryRoute);
 
 export default adminRoute;
