8f43b8d94d28b442448c2a5c66c943c9500dbbaa
Amaan63 <amaansayed63@gmail.com>
2025-09-05 23:37:46 +0530

    created Admin route for category

---
+++ backend/src/routes/admin/category.admin.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { createCategoryController } from "../../controllers/category.controller.js";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../../middlewares/authorize.role.js";
+import { ROLES } from "../../utils/roles.js";
+
+const categoryAdminRoute = express.Router();
+
+// Route to create a new category
+categoryAdminRoute.post(
+  "/createCategory",
+  authMiddleware,
+  authorizeRoles(ROLES.ADMIN),
+  createCategoryController
+); // POST /api/categories
+
+export default categoryAdminRoute;
