1500c035b0b6ef1d834f042fff1673f71a693c93
Amaan63 <amaansayed63@gmail.com>
2025-09-05 23:06:52 +0530

    created post model

---
+++ backend/src/models/post.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    caption: { type: String, required: true },
+    mediaUrl: { type: String, trim: true }, // image or video
+    categories: [
+      {
+        categoryId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Category",
+          required: true,
+        },
+        name: { type: String, required: true }, // for quick search
+      },
+    ],
+    poll: { type: mongoose.Schema.Types.ObjectId, ref: "Poll" }, // optional
+    visibility: {
+      type: String,
+      enum: ["public", "private"],
+      default: "public",
+    },
+    allowComments: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
