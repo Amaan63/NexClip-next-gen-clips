c915989f14b39a4089ceb490579224517e123dba
Amaan63 <amaansayed63@gmail.com>
2025-09-05 23:54:58 +0530

    created Updatecategory Route

--- backend/src/routes/admin/category.admin.route.js
+++ backend/src/routes/admin/category.admin.route.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { createCategoryController } from "../../controllers/category.controller.js";
+import {
+  createCategoryController,
+  deleteCategoryController,
+  updateCategoryController,
+} from "../../controllers/category.controller.js";
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
 import { authorizeRoles } from "../../middlewares/authorize.role.js";
 import { ROLES } from "../../utils/roles.js";
@@ -13,5 +17,14 @@ categoryAdminRoute.post(
   authorizeRoles(ROLES.ADMIN),
   createCategoryController
 ); // POST /api/categories
+
+// Update
+categoryAdminRoute.put(
+  "/updateCategory/:categoryId",
+  authMiddleware,
+  authorizeRoles(ROLES.ADMIN),
+  updateCategoryController
+);
+
 
 export default categoryAdminRoute;
