6e0ac360269c46a33968c191fdf52529017d4099
Amaan63 <amaansayed63@gmail.com>
2025-09-05 20:50:44 +0530

    added admin route

--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -1,11 +1,14 @@
 import express from "express";
 import authRoute from "./auth.route.js";
 import userRoute from "./user.route.js";
+import adminRoute from "./admin.route.js";
 const router = express.Router();
 
 // Auth route
 router.use("/auth", authRoute);
 // User route
 router.use("/user", userRoute);
+// Admin route
+router.use("/admin", adminRoute);
 
 export default router;
