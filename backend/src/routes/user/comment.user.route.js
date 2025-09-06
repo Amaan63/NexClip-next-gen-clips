18c68a5d02ba743229a8e3315a8f6f5119b4298d
Amaan63 <amaansayed63@gmail.com>
2025-09-06 22:26:58 +0530

    added getCommenst Route

--- backend/src/routes/user/comment.user.route.js
+++ backend/src/routes/user/comment.user.route.js
@@ -1,9 +1,18 @@
 import express from "express";
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
-import { addCommentController } from "../../controllers/comment.controller.js";
+import {
+  addCommentController,
+  getCommentsController,
+} from "../../controllers/comment.controller.js";
 
 const userCommentRouter = express.Router();
 
 userCommentRouter.post("/addComment", authMiddleware, addCommentController);
+
+userCommentRouter.get(
+  "/getComments/:postId",
+  authMiddleware,
+  getCommentsController
+);
 
 export default userCommentRouter;
