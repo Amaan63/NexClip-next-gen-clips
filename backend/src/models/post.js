47cd17bcb1aa0f1ac7d2d6a703abaec54892b8f4
Amaan63 <amaansayed63@gmail.com>
2025-09-06 13:30:01 +0530

    changed poll type to Mixed to store object id or string

--- backend/src/models/post.js
+++ backend/src/models/post.js
@@ -14,7 +14,7 @@ const postSchema = new mongoose.Schema(
         name: { type: String, required: true }, // for quick search
       },
     ],
-    poll: { type: mongoose.Schema.Types.ObjectId, ref: "Poll" }, // optional
+    poll: { type: mongoose.Schema.Types.Mixed, ref: "Poll" }, // optional
     visibility: {
       type: String,
       enum: ["public", "private"],
