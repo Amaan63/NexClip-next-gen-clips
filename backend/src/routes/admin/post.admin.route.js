915139efd841cd8a17acba3442d2e06ec31ed77b
Amaan63 <amaansayed63@gmail.com>
2025-09-06 19:32:18 +0530

    added updatePost route in Admin

--- backend/src/routes/admin/post.admin.route.js
+++ backend/src/routes/admin/post.admin.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createPostController,
   getAllPostsForAdminController,
+  updatePostController,
 } from "../../controllers/post.controller.js";
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
 import { authorizeRoles } from "../../middlewares/authorize.role.js";
@@ -21,6 +22,13 @@ postAdminRouter.get(
   authMiddleware,
   authorizeRoles(ROLES.ADMIN),
   getAllPostsForAdminController
+);
+
+postAdminRouter.put(
+  "/updatePost/:postId",
+  authMiddleware,
+  authorizeRoles(ROLES.ADMIN),
+  updatePostController
 );
 
 export default postAdminRouter;
