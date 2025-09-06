28dc4753faef3eea8b0ac95c9ca59659cd39345a
Amaan63 <amaansayed63@gmail.com>
2025-09-06 22:09:18 +0530

    comment user route is created

---
+++ backend/src/routes/user/comment.user.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
+import { addCommentController } from "../../controllers/comment.controller.js";
+
+const userCommentRouter = express.Router();
+
+userCommentRouter.post("/addComment", authMiddleware, addCommentController);
+
+export default userCommentRouter;
