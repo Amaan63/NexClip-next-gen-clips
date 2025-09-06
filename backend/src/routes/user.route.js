fb037cf3ec24853815341cc4976807db2351b5ee
Amaan63 <amaansayed63@gmail.com>
2025-09-06 22:09:54 +0530

    added Comment Route

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -3,11 +3,13 @@ import { getProfile } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import categoryUserRoute from "./user/category.user.route.js";
 import userPostRouter from "./user/post.user.route.js";
+import userCommentRouter from "./user/comment.user.route.js";
 
 const userRoute = express.Router();
 
 userRoute.get("/profile", authMiddleware, getProfile);
 userRoute.use("/", categoryUserRoute); // GET /api/users/profile
 userRoute.use("/Post", userPostRouter); // GET /api/users/Post/getAllPublicPosts
+userRoute.use("/Comment", userCommentRouter); // POST /api/users/Comment/addComment
 
 export default userRoute;
