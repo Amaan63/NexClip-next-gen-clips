e2b8248400583535f1d1c72e25c9082056f04d43
Amaan63 <amaansayed63@gmail.com>
2025-09-07 20:29:04 +0530

    added reels route

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -4,6 +4,7 @@ import { authMiddleware } from "../middlewares/auth.middleware.js";
 import categoryUserRoute from "./user/category.user.route.js";
 import userPostRouter from "./user/post.user.route.js";
 import userCommentRouter from "./user/comment.user.route.js";
+import reelUserRoute from "./user/reel.user.route.js";
 
 const userRoute = express.Router();
 
@@ -11,5 +12,6 @@ userRoute.get("/profile", authMiddleware, getProfile);
 userRoute.use("/", categoryUserRoute); // GET /api/users/profile
 userRoute.use("/Post", userPostRouter); // GET /api/users/Post/getAllPublicPosts
 userRoute.use("/Comment", userCommentRouter); // POST /api/users/Comment/addComment
+userRoute.use("/Reel", reelUserRoute); // GET /api/users/Reel/getAllPublicReels
 
 export default userRoute;
