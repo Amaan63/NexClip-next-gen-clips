66e876147b307e11fbfb0f55b6a445bbbe66d2f0
Amaan63 <amaansayed63@gmail.com>
2025-09-12 23:06:25 +0530

    Toast Provider is Wrapped to the App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
+import { ToastProvider } from "./components/Toast/ToastProvider";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <ToastProvider>
+      <RouterProvider router={router} />
+    </ToastProvider>
+  );
 };
 
 export default App;
