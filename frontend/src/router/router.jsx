fa2c92c4273e583fa3dde2f38a03b97a6e455170
Amaan63 <amaansayed63@gmail.com>
2025-09-15 22:23:06 +0530

    added the multiple routes

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,10 +1,35 @@
 import { createBrowserRouter } from "react-router-dom";
+
+// Existing pages
 import Login from "../pages/Login";
 import NotFound from "../pages/NotFound";
+
+// New pages
+import Home from "../pages/Home";
+import Search from "../pages/Search";
+import Reels from "../pages/Reels";
+import Profile from "../pages/Profile";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />, // show login at root
+    element: <Login />, // Login page at root
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
+  },
+  {
+    path: "/reels",
+    element: <Reels />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
   },
   {
     path: "*", // catch-all 404
