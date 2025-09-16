9d345f0c8efd26371d7839fefcc66da59f411bc0
Amaan63 <amaansayed63@gmail.com>
2025-09-16 20:25:15 +0530

    created auth slice

---
+++ frontend/src/redux/features/auth/auth.slice.js
@@ -0,0 +1,45 @@
+// src/redux/features/auth/authSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+import { loginUser } from "./auth.thunk";
+
+const initialState = {
+  user: null,
+  role: null,
+  token: sessionStorage.getItem("token") || null,
+  loading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.user = null;
+      state.role = null;
+      state.token = null;
+      state.error = null;
+      sessionStorage.removeItem("token");
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+        state.role = action.payload.role;
+        state.token = action.payload.token;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
