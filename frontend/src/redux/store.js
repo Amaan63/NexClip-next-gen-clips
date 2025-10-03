c03e6314b9adfea69ea6b4887596435f322ff164
Amaan63 <amaansayed63@gmail.com>
2025-10-03 20:14:03 +0530

    added categoryReducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 // src/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/auth.slice";
+import categoryReducer from "./features/category/category.slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    category: categoryReducer,
   },
 });
