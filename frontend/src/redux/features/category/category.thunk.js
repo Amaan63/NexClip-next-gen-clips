dd13dfe869e8244864b448acb8de89c7ba3c38bd
Amaan63 <amaansayed63@gmail.com>
2025-10-03 20:45:43 +0530

    created fetchAllCategoriesThunk to manage the response

--- frontend/src/redux/features/category/category.thunk.js
+++ frontend/src/redux/features/category/category.thunk.js
@@ -1,11 +1,28 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { createCategory } from "./category.api";
+import { createCategory, getAllCategories } from "./category.api";
 
 export const createCategoryThunk = createAsyncThunk(
   "category/createCategory",
   async (categoryData, { rejectWithValue }) => {
     try {
       const data = await createCategory(categoryData);
+      if (!data.success) {
+        return rejectWithValue(data.message);
+      }
+      return data.categories;
+    } catch (err) {
+      return rejectWithValue(
+        err.response?.data?.message || "Something went wrong"
+      );
+    }
+  }
+);
+
+export const fetchAllCategoriesThunk = createAsyncThunk(
+  "category/fetchAllCategories",
+  async (_, { rejectWithValue }) => {
+    try {
+      const data = await getAllCategories();
       if (!data.success) {
         return rejectWithValue(data.message);
       }
