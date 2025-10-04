0ae44fa5850cdc31abe7398da2fa6d53471af8aa
Amaan63 <amaansayed63@gmail.com>
2025-10-04 19:15:22 +0530

    added getAllPoststhunk method to manage the response

--- frontend/src/redux/features/post/post.thunk.js
+++ frontend/src/redux/features/post/post.thunk.js
@@ -1,11 +1,25 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { createPost } from "./post.api";
+import { createPost, getAllPosts } from "./post.api";
 
 export const createPostThunk = createAsyncThunk(
   "post/createPost",
   async (postData, { rejectWithValue }) => {
     try {
       const response = await createPost(postData);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const getAllPostsThunk = createAsyncThunk(
+  "post/getAllPosts",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await getAllPosts();
+      console.log("Fetched posts:", response);
+
       return response;
     } catch (error) {
       return rejectWithValue(error.response.data);
