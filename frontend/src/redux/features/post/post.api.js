632b1f8fdd3875e9096edd2256c0a6625f6e5d22
Amaan63 <amaansayed63@gmail.com>
2025-10-04 19:14:56 +0530

    added getAllPosts method to call the api

--- frontend/src/redux/features/post/post.api.js
+++ frontend/src/redux/features/post/post.api.js
@@ -4,3 +4,8 @@ export const createPost = async (postData) => {
   const { data } = apiClient.post(`admin/Post/createPost`, postData);
   return data;
 };
+
+export const getAllPosts = async () => {
+  const { data } = await apiClient.get(`admin/Post/getAllPosts`);
+  return data;
+};
