12ef9bf16d1e739536a36ecd38d750bc9d391f1b
Amaan63 <amaansayed63@gmail.com>
2025-10-04 11:56:53 +0530

    added post reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/auth.slice";
 import categoryReducer from "./features/category/category.slice";
+import postReducer from "./features/post/post.slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     category: categoryReducer,
+    post: postReducer,
   },
 });
